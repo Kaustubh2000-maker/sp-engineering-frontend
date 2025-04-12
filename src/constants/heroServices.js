64197637a4ce9ca39997c883cb178c39056d5fa6
Kaustubh Chaudhari <kaustubh01a.c@gmail.com>
2025-04-12 21:45:35 +0530

    resposive

--- src/constants/heroServices.js
+++ src/constants/heroServices.js
@@ -19,34 +19,34 @@ export const HERO_SERVICES = [
       path: "/products/fire",
     },
   },
-  // {
-  //   id: "solar",
-  //   name: "solar",
-  //   title: "Your business Needs Our",
-  //   description:
-  //     "Transition to solar and take control of your energy expenses. Our commercial solar solutions offer significant cost savings by harnessing clean and renewable power, making your business more energy-efficient and environmentally responsible.",
-  //   image: solarImage,
-  //   alignment: "right",
-  //   color: "#21549c",
-  //   button: {
-  //     label: "Learn More",
-  //     path: "/products/solar",
-  //   },
-  // },
-  // {
-  //   id: "hvac",
-  //   name: "hvac",
-  //   title: "Optimized Climate Control for Industrial Environments",
-  //   description:
-  //     "Enhance productivity with our state-of-the-art industrial HVAC systems. Designed for factories, warehouses, and large facilities, our solutions provide efficient climate control and optimal air quality.",
-  //   image: hvacImage,
-  //   alignment: "left",
-  //   color: "#96c2ff",
-  //   button: {
-  //     label: "Learn More",
-  //     path: "/products/hvac",
-  //   },
-  // },
+  {
+    id: "solar",
+    name: "solar",
+    title: "Your business Needs Our",
+    description:
+      "Transition to solar and take control of your energy expenses. Our commercial solar solutions offer significant cost savings by harnessing clean and renewable power, making your business more energy-efficient and environmentally responsible.",
+    image: solarImage,
+    alignment: "right",
+    color: "#21549c",
+    button: {
+      label: "Learn More",
+      path: "/products/solar",
+    },
+  },
+  {
+    id: "hvac",
+    name: "hvac",
+    title: "Optimized Climate Control for Industrial Environments",
+    description:
+      "Enhance productivity with our state-of-the-art industrial HVAC systems. Designed for factories, warehouses, and large facilities, our solutions provide efficient climate control and optimal air quality.",
+    image: hvacImage,
+    alignment: "left",
+    color: "#96c2ff",
+    button: {
+      label: "Learn More",
+      path: "/products/hvac",
+    },
+  },
 ];
 
 export const ENERGY_IMAGE = "@assets/heroSection/hero-energy-name-1.png";
