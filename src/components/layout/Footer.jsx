b077b0880461e514675de8f499239372888e2ed0
Kaustubh Chaudhari <kaustubh01a.c@gmail.com>
2025-05-23 11:33:24 +0530

    footer set

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -7,34 +7,69 @@ function Footer() {
         <div className="footer-logo-div">
           <img className="footer-logo" src={footerLogo} alt="" />
           <div className="footer-icon-div">
-            <ion-icon className="footer-icon" name="logo-instagram"></ion-icon>
-            <ion-icon className="footer-icon" name="logo-facebook"></ion-icon>
-            <ion-icon className="footer-icon" name="logo-whatsapp"></ion-icon>
-            <ion-icon className="footer-icon" name="logo-twitter"></ion-icon>
+            <ion-icon class="footer-icon" name="logo-instagram"></ion-icon>
+            <ion-icon class="footer-icon" name="logo-facebook"></ion-icon>
+            <ion-icon class="footer-icon" name="logo-whatsapp"></ion-icon>
+            <ion-icon class="footer-icon" name="logo-twitter"></ion-icon>
           </div>
         </div>
-        <div className="footer-address footer-div">
+        {/* <div className="footer-address footer-div">
           <h3 className="footer-heading">Contact us</h3>
           <ul className="footer-ul">
             <li className="footer-li">this is li</li>
             <li className="footer-li">this is li</li>
             <li className="footer-li">this is li</li>
           </ul>
-        </div>
+        </div> */}
         <div className="footer-contact footer-div">
           <h3 className="footer-heading">Address</h3>
           <ul className="footer-ul">
-            <li className="footer-li">this is li</li>
-            <li className="footer-li">this is li</li>
-            <li className="footer-li">this is li</li>
+            <li className="footer-li">
+              <div>
+                <ion-icon
+                  class="footer-sub-icon"
+                  name="location-outline"
+                ></ion-icon>
+                <span>Nashik Office</span>
+              </div>
+              8A, Kala Nagar, Mhada colony, Nashik, MH-422004
+            </li>
+            <li className="footer-li">
+              <div>
+                <ion-icon
+                  class="footer-sub-icon"
+                  name="location-outline"
+                ></ion-icon>
+                <span>Vadodara Office</span>
+              </div>
+              A/11, Rudrakhs County, Nr. L&T Knowledge City, N.H. 8, Vadodara,
+              GJ – 390019
+            </li>
           </ul>
         </div>
         <div className="footer-about footer-div">
-          <h3 className="footer-heading">Company</h3>
+          <h3 className="footer-heading">Contact</h3>
           <ul className="footer-ul">
-            <li className="footer-li">this is li</li>
-            <li className="footer-li">this is li</li>
-            <li className="footer-li">this is li</li>
+            <li className="footer-li">
+              <div>
+                <ion-icon
+                  class="footer-sub-icon"
+                  name="mail-outline"
+                ></ion-icon>
+                <span className="footer-contact-span">Email</span>
+              </div>
+              rohitspengg@gmail.com
+            </li>
+            <li className="footer-li">
+              <div>
+                <ion-icon
+                  class="footer-sub-icon"
+                  name="call-outline"
+                ></ion-icon>
+                <span className="footer-contact-span">Contact no.</span>
+              </div>
+              +91 89831 61405
+            </li>
           </ul>
         </div>
       </div>
