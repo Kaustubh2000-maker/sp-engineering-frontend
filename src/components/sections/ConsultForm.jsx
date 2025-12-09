44003ee52fcd0054eaff1c2b4c09f1d9fbb33e56
Kaustubh Chaudhari <kaustubh01a.c@gmail.com>
2025-12-09 10:20:44 +0530

    consult form

--- src/components/sections/ConsultForm.jsx
+++ src/components/sections/ConsultForm.jsx
@@ -1,17 +1,198 @@
+// import React, { useState } from "react";
+// import { serviceConfig } from "@/constants/consultForm";
+// import { motion } from "framer-motion";
+// import {
+//   nrmlScaleUp,
+//   nrmlLeft,
+//   nrmlRight,
+//   nrmlVisible,
+// } from "@/animations/appAnimation";
+
+// function ConsultForm({ service, product }) {
+//   const usageOptions = Object.keys(serviceConfig[service] || {});
+//   const [usageType, setUsageType] = useState(usageOptions[0] || "");
+//   const fields = usageType ? serviceConfig[service][usageType] : [];
+//   return (
+//     <div className="consult-form-section">
+//       <div className="consult-form-bg">
+//         <img className="consult-form-bg-img" src={product.image} alt="" />
+//       </div>
+
+//       <div className="consult-form-section-grid">
+//         <div className="consult-content-div">
+//           <motion.h1 className="consult-content-heading" {...nrmlVisible()}>
+//             {product.title}
+//           </motion.h1>
+//           <motion.img
+//             className="consult-content-img"
+//             {...nrmlScaleUp(0.2)}
+//             src={product.image}
+//             alt=""
+//           />
+//           <motion.p className="consult-content-desc" {...nrmlScaleUp(0.3)}>
+//             {product.description[0]}
+//           </motion.p>
+//           <motion.p className="consult-content-desc" {...nrmlScaleUp(0.4)}>
+//             {product.description[1]}
+//           </motion.p>
+//         </div>
+
+//         <motion.div className="consult-form-div" {...nrmlVisible()}>
+//           <h2 className="consult-form-heading">Inquiry Form</h2>
+
+//           {/* Usage Type Selector as Flex Buttons */}
+//           {usageOptions.length > 0 && (
+//             <div className="consult-form-feild-div">
+//               {usageOptions.map((type) => (
+//                 <button
+//                   className={`consult-form-feild-type-btn ${
+//                     usageType === type
+//                       ? "consult-form-feild-selected-usage-type"
+//                       : ""
+//                   }`}
+//                   key={type}
+//                   type="button"
+//                   onClick={() => setUsageType(type)}
+//                 >
+//                   {type}
+//                 </button>
+//               ))}
+//             </div>
+//           )}
+
+//           {/* Dynamic Form Fields */}
+//           <form className="consult-form-box">
+//             <div className="consult-form-label-input-div">
+//               <label className="consult-form-label">
+//                 Name <span className="cta-required">*</span>{" "}
+//               </label>
+//               <input
+//                 type="text"
+//                 placeholder="Name"
+//                 className="consult-form-input"
+//               />
+//             </div>
+//             <div className="consult-form-label-input-div">
+//               <label className="consult-form-label">
+//                 {usageType === "Commercial"
+//                   ? "Company Name"
+//                   : usageType === "Industrial"
+//                   ? "Factory / Plant Name"
+//                   : usageType === "HousingSociety"
+//                   ? "Society Name"
+//                   : usageType === "Residential"
+//                   ? "Building Name"
+//                   : "Organization Name"}
+//                 <span className="cta-required">*</span>{" "}
+//               </label>
+//               <input
+//                 type="text"
+//                 placeholder="Enter Name"
+//                 className="consult-form-input"
+//               />
+//             </div>
+//             <div className="consult-from-contact-pincode-div">
+//               <div className="consult-form-label-input-div">
+//                 <label className="consult-form-label">
+//                   Contact Number <span className="cta-required">*</span>{" "}
+//                 </label>
+//                 <input
+//                   type="text"
+//                   placeholder="Contact Number"
+//                   className="consult-form-input"
+//                 />
+//               </div>
+
+//               <div className="consult-form-label-input-div">
+//                 <label className="consult-form-label">
+//                   Pincode <span className="cta-required">*</span>{" "}
+//                 </label>
+//                 <input
+//                   type="text"
+//                   placeholder="Pincode"
+//                   className="consult-form-input"
+//                 />
+//               </div>
+//             </div>
+//             {fields.map((field, index) => (
+//               <div key={index} className="consult-form-label-input-div">
+//                 <label className="consult-form-label">
+//                   {field.label} <span className="cta-required">*</span>{" "}
+//                 </label>
+//                 {field.type === "select" ? (
+//                   <select className="consult-form-select">
+//                     <option
+//                       className="consult-form-option consult-form-option-default"
+//                       value=""
+//                     >
+//                       --Select--
+//                     </option>
+//                     {field.options.map((opt, i) => (
+//                       <option
+//                         key={i}
+//                         value={opt}
+//                         className="consult-form-option"
+//                       >
+//                         {opt}
+//                       </option>
+//                     ))}
+//                   </select>
+//                 ) : (
+//                   <input
+//                     type={field.type}
+//                     placeholder={field.label}
+//                     className="consult-form-input"
+//                   />
+//                 )}
+//               </div>
+//             ))}
+
+//             <button type="submit" className="consult-form-submit-btn">
+//               Submit
+//             </button>
+//           </form>
+//         </motion.div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default ConsultForm;
+
 import React, { useState } from "react";
 import { serviceConfig } from "@/constants/consultForm";
 import { motion } from "framer-motion";
-import {
-  nrmlScaleUp,
-  nrmlLeft,
-  nrmlRight,
-  nrmlVisible,
-} from "@/animations/appAnimation";
+import { nrmlScaleUp, nrmlVisible } from "@/animations/appAnimation";
 
 function ConsultForm({ service, product }) {
   const usageOptions = Object.keys(serviceConfig[service] || {});
   const [usageType, setUsageType] = useState(usageOptions[0] || "");
   const fields = usageType ? serviceConfig[service][usageType] : [];
+
+  // =============== WEB3FORMS SUBMIT HANDLER ===============
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+    formData.append("access_key", "7fdf2b89-9f0a-4289-8086-771195fb29e0");
+    formData.append("usage_type", usageType);
+    formData.append("product", product.title);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const result = await response.json();
+
+    if (result.success) {
+      alert("Message Sent Successfully!");
+      e.target.reset();
+    } else {
+      alert("Failed to send message.");
+    }
+  };
+
   return (
     <div className="consult-form-section">
       <div className="consult-form-bg">
@@ -19,40 +200,45 @@ function ConsultForm({ service, product }) {
       </div>
 
       <div className="consult-form-section-grid">
+        {/* PRODUCT DETAIL SECTION */}
         <div className="consult-content-div">
           <motion.h1 className="consult-content-heading" {...nrmlVisible()}>
             {product.title}
           </motion.h1>
+
           <motion.img
             className="consult-content-img"
             {...nrmlScaleUp(0.2)}
             src={product.image}
             alt=""
           />
+
           <motion.p className="consult-content-desc" {...nrmlScaleUp(0.3)}>
             {product.description[0]}
           </motion.p>
+
           <motion.p className="consult-content-desc" {...nrmlScaleUp(0.4)}>
             {product.description[1]}
           </motion.p>
         </div>
 
+        {/* FORM SECTION */}
         <motion.div className="consult-form-div" {...nrmlVisible()}>
           <h2 className="consult-form-heading">Inquiry Form</h2>
 
-          {/* Usage Type Selector as Flex Buttons */}
+          {/* Usage Selector */}
           {usageOptions.length > 0 && (
             <div className="consult-form-feild-div">
               {usageOptions.map((type) => (
                 <button
+                  key={type}
+                  type="button"
+                  onClick={() => setUsageType(type)}
                   className={`consult-form-feild-type-btn ${
                     usageType === type
                       ? "consult-form-feild-selected-usage-type"
                       : ""
                   }`}
-                  key={type}
-                  type="button"
-                  onClick={() => setUsageType(type)}
                 >
                   {type}
                 </button>
@@ -60,18 +246,23 @@ function ConsultForm({ service, product }) {
             </div>
           )}
 
-          {/* Dynamic Form Fields */}
-          <form className="consult-form-box">
+          {/* FORM START */}
+          <form className="consult-form-box" onSubmit={handleSubmit}>
+            {/* Name */}
             <div className="consult-form-label-input-div">
               <label className="consult-form-label">
-                Name <span className="cta-required">*</span>{" "}
+                Name <span className="cta-required">*</span>
               </label>
               <input
+                name="name"
                 type="text"
                 placeholder="Name"
                 className="consult-form-input"
+                required
               />
             </div>
+
+            {/* Company / Factory / Building */}
             <div className="consult-form-label-input-div">
               <label className="consult-form-label">
                 {usageType === "Commercial"
@@ -83,70 +274,80 @@ function ConsultForm({ service, product }) {
                   : usageType === "Residential"
                   ? "Building Name"
                   : "Organization Name"}
-                <span className="cta-required">*</span>{" "}
+                <span className="cta-required">*</span>
               </label>
+
               <input
+                name="organization_name"
                 type="text"
                 placeholder="Enter Name"
                 className="consult-form-input"
+                required
               />
             </div>
+
+            {/* Contact + Pincode */}
             <div className="consult-from-contact-pincode-div">
               <div className="consult-form-label-input-div">
                 <label className="consult-form-label">
-                  Contact Number <span className="cta-required">*</span>{" "}
+                  Contact Number <span className="cta-required">*</span>
                 </label>
                 <input
+                  name="contact_number"
                   type="text"
                   placeholder="Contact Number"
                   className="consult-form-input"
+                  required
                 />
               </div>
 
               <div className="consult-form-label-input-div">
                 <label className="consult-form-label">
-                  Pincode <span className="cta-required">*</span>{" "}
+                  Pincode <span className="cta-required">*</span>
                 </label>
                 <input
+                  name="pincode"
                   type="text"
                   placeholder="Pincode"
                   className="consult-form-input"
+                  required
                 />
               </div>
             </div>
+
+            {/* DYNAMIC FIELDS */}
             {fields.map((field, index) => (
               <div key={index} className="consult-form-label-input-div">
                 <label className="consult-form-label">
-                  {field.label} <span className="cta-required">*</span>{" "}
+                  {field.label} <span className="cta-required">*</span>
                 </label>
+
                 {field.type === "select" ? (
-                  <select className="consult-form-select">
-                    <option
-                      className="consult-form-option consult-form-option-default"
-                      value=""
-                    >
-                      --Select--
-                    </option>
+                  <select
+                    name={field.label}
+                    className="consult-form-select"
+                    required
+                  >
+                    <option value="">--Select--</option>
                     {field.options.map((opt, i) => (
-                      <option
-                        key={i}
-                        value={opt}
-                        className="consult-form-option"
-                      >
+                      <option key={i} value={opt}>
                         {opt}
                       </option>
                     ))}
                   </select>
                 ) : (
                   <input
+                    name={field.label}
                     type={field.type}
                     placeholder={field.label}
                     className="consult-form-input"
+                    required
                   />
                 )}
               </div>
             ))}
 
+            {/* SUBMIT */}
             <button type="submit" className="consult-form-submit-btn">
               Submit
             </button>
